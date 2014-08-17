37aa1ae004fe34c4602be9d136b1c212101b5121
Mohanraj Chinnasamy <cmohanraj2000@yahoo.com>
2014-08-16 21:41:18 -0700

    R Programing course - Assigment2 submission - 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script make use of the R caching mechanism to find out the inverse of a 
+## invertible square matrix. 
 
-## Write a short comment describing this function
+## This function creates/returns a special list with the following functions 
+## setMatrix(y)         - set the matrix y to x
+## getMatrix()          - get the matrix x
+## setInverse(inverse)  - set the inverse to inverse_matrix
+## getInverse()         - get the inverse of the matrix x, inverse_matrix
+## Parameter - input    - x, a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## intialize the resultant matrix
+        inverse_matrix <- NULL
+        
+        ## the setMatrix function assigns the input matrix 'y' to 'x'
+        setMatrix <- function(y)
+        { 
+                ## initialize again as new matrix y is assign to 'x'
+                inverse_matrix <<- NULL
+        }
+        
+        ## the getMatrix function returns the matrix 'x'
+        getMatrix <- function()
+        {
+                x                
+        }
+        
+        ## the setInverse function assigns the input inverse to inverse_matrix
+        setInverse <- function (inverse)
+        {
+                inverse_matrix <<- inverse       
+        }
+        
+        ## the getInverse function returns the inverse_matrix
+        getInverse <- function()
+        {
+                inverse_matrix
+        }
+        
+        ## returns the list with the four functions
+        list(getMatrix = getMatrix, setMatrix = setMatrix, 
+             getInverse = getInverse, setInverse = setInverse)
 }
 
-
-## Write a short comment describing this function
+## This function returns inverse of the given invertible square matrix 'x'.
+## it first checks in the cache, and if it is not found then it calculates 
+## the inverse using solve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inverse <- x$getInverse()
+        
+        ## checks if it is in cache or not. if it is then return from cache
+        if(!is.null(inverse))
+        {
+                message("getting cashed data")
+                
+                ## retruns the cache
+                return(inverse)
+        }
+        
+        ## else part
+        
+        data <- x$getMatrix()
+        
+        ## calculate the inverse using the solve function
+        inverse <- solve(data)
+        
+        ## set the inverse to the cache
+        x$setInverse(inverse)
+        
+        ## return the inverse
+        inverse      
 }
